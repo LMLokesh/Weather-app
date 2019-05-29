b506068acc502ae8b7eb1df3e6fedc45b034d28e
Lokesh Mareppa <lokesh.mareppa@d01005564.ptv.com>
2019-05-29 04:00:28 +0100

    Updating forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,14 +3,18 @@ const request = require("request");
 const forecast = (latitude, longitude, callback) => {
     // Getting Weather details using latitude and longitude from DarkSky
     const darkSkyApi = `https://api.darksky.net/forecast/1d1978cece25de83e6ee419af38454fe/${latitude},${longitude}?units=auto`
-    request({url: darkSkyApi, json: true}, (err, response) => {
+    request({url: darkSkyApi, json: true}, (err, {body}) => {
         if(err){
             callback('Unable to connect to weather service!', undefined)
-        } else if (response.body.error){
+        } else if (body.error){
             callback('Unable to find location', undefined);
         } else {
-            const currently = response.body.currently;
-            const data = `${response.body.daily.data[0].summary} It is currently ${currently.temperature} degrees out. this high today is ${body.daily.data[0].temperatureHigh} with a low of ${body.daily.data[0].temperatureLow}. There is a ${currently.precipProbability}% of rain`
+            const currently = body.currently;
+            const data = 
+                `${body.daily.data[0].summary} 
+                It is currently ${currently.temperature} degrees out. 
+                This high today is ${body.daily.data[0].temperatureHigh} with a low of ${body.daily.data[0].temperatureLow}. 
+                There is a ${currently.precipProbability}% of rain`
             callback(false, data)
         }
     })
